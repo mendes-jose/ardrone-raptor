c86755e2bdb3ecd018166c12ceb5d4bd4e624bb1
Jose Magno MENDES FILHO <jmagno.mf@gmail.com>
2015-02-16 10:25:18 +0100

    first meaningless build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gandalf/Dev/ardrone-raptor/src
+# Build directory: /home/gandalf/Dev/ardrone-raptor/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/gandalf/Dev/ardrone-raptor/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/gandalf/Dev/ardrone-raptor/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/gandalf/Dev/ardrone-raptor/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/gandalf/Dev/ardrone-raptor/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/gandalf/Dev/ardrone-raptor/devel/lib:/home/gandalf/Dev/ardrone-raptor/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/gandalf/Dev/ardrone-raptor/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/gandalf/Dev/ardrone-raptor/devel/lib/pkgconfig:/home/gandalf/Dev/ardrone-raptor/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/gandalf/Dev/ardrone-raptor/build"
+export PYTHONPATH="/home/gandalf/Dev/ardrone-raptor/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/gandalf/Dev/ardrone-raptor/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/gandalf/Dev/ardrone-raptor/src:$ROS_PACKAGE_PATH"
